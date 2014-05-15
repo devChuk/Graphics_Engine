1bc36d1b99ae0379dad72b55776fc2ee24066707
Brian Chuk <brian_chuk@yahoo.com>
2014-05-15 11:21:52 -0400

    line setup complete

---
+++ parts/line/draw.py
@@ -0,0 +1,18 @@
+from display import *
+from matrix import *
+
+
+#Go through matrix 2 entries at a time and call
+#draw_line on each pair of ponts
+def draw_lines( matrix, screen, color ):
+
+#Add the edge (x0, y0, z0) - (x1, y1, z1) to matrix
+def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
+
+#Add the point (x, y, z) to matrix
+def add_point( matrix, x, y, z=0 ):
+
+#Plot all the pixels needed to draw line (x0, y0) - (x1, y1)
+#to screen with color
+def draw_line( screen, x0, y0, x1, y1, color ):
+
